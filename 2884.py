822c4ad9e4d2b570b6f4f434d408a52ec86e57fa
jeongho0715 <jeongho0715@outlook.com>
2024-03-03 04:59:48 +0900

    fix: fix error when m=45

--- 2884.py
+++ 2884.py
@@ -1,6 +1,12 @@
 h, m = map(int, input().split())
 
-if h > 0:
-    print(h-1, 60-abs(m-45))
+if m != 45:
+    if h > 0:
+        print(h - 1, 60 - abs(m - 45))
+    else:
+        print(23, 60 - abs(m - 45))
 else:
-    print(23, 60-abs(m-45))
+    if h > 0:
+        print(h - 1, 0)
+    else:
+        print(23, 0)
