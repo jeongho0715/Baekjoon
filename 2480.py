0345d4b057ec8d79a09964d985d06af659aa996c
jeongho0715 <jeongho0715@outlook.com>
2024-03-03 18:21:37 +0900

    create: solve test #2480

---
+++ 2480.py
@@ -0,0 +1,14 @@
+a, b, c = map(int, input().split())
+prize = 0
+
+if a == b == c:
+    prize = 10000 + a*1000
+elif a != b != c:
+    prize = max(a, b, c)*100
+else:
+    if a == b or a == c:
+        prize = 1000 + a*100
+    elif b == c:
+        prize = 1000 + b*100
+
+print(prize)
